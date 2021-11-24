f7c261ec5a054109b03893dd3110798878ce162e
frame88 <fleuzzi97@gmail.com>
2021-11-24 17:59:22 +0100

    pareggio e numeri compresi tra 1 e 6, in DADI

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,9 +1,12 @@
-let numpc = Math.random();
-let numuser = Math.random();
+let numpc = Math.floor((Math.random() * 6) + 1);
+let numuser = Math.floor((Math.random() * 6) + 1);
 console.log(numuser,numpc);
 
 if (numpc > numuser) {
     console.log('ha vinto il computer');
+}
+if (numpc == numuser) {
+    console.log('pareggio');
 }
 else {
     console.log('hai vinto tu');
