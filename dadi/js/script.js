342b7e09de2ef7fb940d233ec608cc4cf768af4f
frame88 <fleuzzi97@gmail.com>
2021-11-24 18:16:44 +0100

    rifiniture dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,13 +1,17 @@
 let numpc = Math.floor((Math.random() * 6) + 1);
 let numuser = Math.floor((Math.random() * 6) + 1);
 console.log(numuser,numpc);
+let risultato = document.querySelector(".container");
 
 if (numpc > numuser) {
     console.log('ha vinto il computer');
+    risultato.append('ha vinto il pc con ',numpc);
 }
-if (numpc == numuser) {
+else if (numpc == numuser) {
     console.log('pareggio');
+    risultato.append('pareggio ',numpc,'-',numuser);
 }
 else {
     console.log('hai vinto tu');
+    risultato.append('hai vinto tu con ',numuser);
 }
