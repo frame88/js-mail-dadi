4d16c1913289905f5cb8491eeadcbca910d5e525
frame88 <fleuzzi97@gmail.com>
2021-11-24 17:52:28 +0100

    complete

---
+++ dadi/js/script.js
@@ -0,0 +1,10 @@
+let numpc = Math.random();
+let numuser = Math.random();
+console.log(numuser,numpc);
+
+if (numpc > numuser) {
+    console.log('ha vinto il computer');
+}
+else {
+    console.log('hai vinto tu');
+}
